c50aef457a1ba0f0378ca42c82d51c42ea878458
Christopher Truman <cleetruman@gmail.com>
2013-11-13 02:48:30 -0800

    Initial MVP built

--- Listen/CLTFirstViewController.h
+++ Listen/CLTFirstViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface CLTFirstViewController : UIViewController
+@interface CLTFirstViewController : UITableViewController
 
 @end
--- Podfile
+++ Podfile
@@ -2,3 +2,6 @@ platform :ios, '7.0'
 inhibit_all_warnings!
 
 pod 'PocketAPI', '~> 1.0.2'
+pod 'AFNetworking'
+pod 'JSONKit'
+
